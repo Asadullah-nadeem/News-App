9029fc01b901dd4ec1c6878013bbdf3bdd305e81
Asadullah-nadeem <88024587+Asadullah-nadeem@users.noreply.github.com>
2024-03-17 05:10:10 +0530

    add some file and update some code

--- login_mobile.php
+++ login_mobile.php
@@ -1,7 +1,7 @@
 <?php
 global $con;
 session_start();
-include('config.php');
+include('./config.php');
 
 // Check if the user is logged in
 if (strlen($_SESSION['login']) == 0) {
@@ -21,8 +21,29 @@ if (strlen($_SESSION['login']) == 0) {
     <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
     <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
     <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="../plugins/switchery/switchery.min.css">
+    <link rel="stylesheet" href="plugins/switchery/switchery.min.css">
     <script src="assets/js/modernizr.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Madimi+One&family=Mohave:ital,wght@0,300..700;1,300..700&family=Nova+Square&family=Roboto+Slab&family=Strait&display=swap" rel="stylesheet">
+    <style>
+        .dark-black {
+            color: #1a202c;
+            font-family: "Madimi One", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+        }
+        @keyframes slideInFromLeft {
+            0% {
+                transform: translateX(-100%);
+            }
+            100% {
+                transform: translateX(0);
+            }
+        }
+    </style>
+
 </head>
 
 <body class="fixed-left">
@@ -39,7 +60,7 @@ if (strlen($_SESSION['login']) == 0) {
     <div class="content-page">
         <!-- Start content -->
         <div class="content">
-            <div class="container">
+            <div class="container mx-auto">
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="page-title-box">
@@ -54,51 +75,50 @@ if (strlen($_SESSION['login']) == 0) {
                 <div class="col-sm-6">
                     <?php if (isset($msg)) { ?>
                         <div class="alert alert-success" role="alert">
-                            <strong>Well done!</strong> <?php echo htmlentities($msg); ?>
+                            <strong class="font-bold">Well done!</strong> <?php echo htmlentities($msg); ?>
                         </div>
                     <?php } ?>
                     <?php if (isset($delmsg)) { ?>
                         <div class="alert alert-danger" role="alert">
-                            <strong>Oh snap!</strong> <?php echo htmlentities($delmsg); ?>
+                            <strong class="font-bold">Oh snap!</strong> <?php echo htmlentities($delmsg); ?>
                         </div>
                     <?php } ?>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-12">
-                    <div class="m-b-30">
-                    </div>
+                    <div class="m-b-30"></div>
                     <div class="table-responsive">
-                        <table class="table m-0 table-colored-bordered table-bordered-primary">
+                        <table class="table m-0 table-bordered border-primary">
                             <thead>
                             <tr>
-                                <th>No.</th>
-                                <th>Unique Id</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Password</th>
-                                <th>Otp</th>
-                                <th>verified</th>
-                                <th>Time</th>
-                                <th>Action</th> <!-- New column for the delete button -->
+                                <th class="px-4 py-2">No.</th>
+                                <th class="px-4 py-2">Unique Id</th>
+                                <th class="px-4 py-2">Name</th>
+                                <th class="px-4 py-2">Email</th>
+                                <th class="px-4 py-2">Password</th>
+                                <th class="px-4 py-2">OTP</th>
+                                <th class="px-4 py-2">verified</th>
+                                <th class="px-4 py-2">Time</th>
+                                <th class="px-4 py-2">Action</th>
                             </tr>
                             </thead>
                             <tbody>
                             <?php
                             $query = mysqli_query($con, "SELECT id, unique_id, name, email, encrypted_password, otp, verified, created_at FROM users");
-                            $cnt = 1; // Initialize the $cnt variable
+                            $cnt = 1;
                             while ($row = mysqli_fetch_array($query)) {
                                 ?>
                                 <tr>
-                                    <th scope="row"><?php echo htmlentities($cnt); ?></th>
-                                    <td><?php echo htmlentities($row['unique_id']); ?></td>
-                                    <td><?php echo htmlentities($row['name']); ?></td>
-                                    <td><?php echo htmlentities($row['email']); ?></td>
-                                    <td><?php echo htmlentities($row['encrypted_password']); ?></td>
-                                    <td><?php echo htmlentities($row['otp']); ?></td>
-                                    <td><?php echo htmlentities($row['verified']); ?></td>
-                                    <td><?php echo htmlentities($row['created_at']); ?></td>
-                                    <td>
+                                    <td class="border px-4 py-2 dark-black"><?php echo htmlentities($cnt); ?></td>
+                                    <td class="border px-4 py-2 dark-black"><?php echo htmlentities($row['unique_id']); ?></td>
+                                    <td class="border px-4 py-2 dark-black"><?php echo htmlentities($row['name']); ?></td>
+                                    <td class="border px-4 py-2 dark-black"><?php echo htmlentities($row['email']); ?></td>
+                                    <td class="border px-4 py-2 dark-black"><?php echo htmlentities($row['encrypted_password']); ?></td>
+                                    <td class="border px-4 py-2 dark-black"><?php echo htmlentities($row['otp']); ?></td>
+                                    <td class="border px-4 py-2 dark-black"><?php echo htmlentities($row['verified']); ?></td>
+                                    <td class="border px-4 py-2 dark-black"><?php echo htmlentities($row['created_at']); ?></td>
+                                    <td class="border px-4 py-2 dark-black">
                                         <a href="delete_user.php?id=<?php echo $row['id']; ?>" onclick="return confirm('Are you sure you want to delete this user?')" >
                                             <i class="fa fa-trash"></i>
                                         </a>
@@ -109,7 +129,6 @@ if (strlen($_SESSION['login']) == 0) {
                             }
                             ?>
                             </tbody>
-
                         </table>
                     </div>
                 </div>
@@ -129,7 +148,7 @@ if (strlen($_SESSION['login']) == 0) {
     // Remove the old script content
     var resizefunc = [];
 </script>
-<script src="export.js"></script>
+<script src="../export.js"></script>
 
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
@@ -139,7 +158,7 @@ if (strlen($_SESSION['login']) == 0) {
 <script src="assets/js/waves.js"></script>
 <script src="assets/js/jquery.slimscroll.js"></script>
 <script src="assets/js/jquery.scrollTo.min.js"></script>
-<script src="../plugins/switchery/switchery.min.js"></script>
+<script src="plugins/switchery/switchery.min.js"></script>
 <!-- App js -->
 <script src="assets/js/jquery.core.js"></script>
 <script src="assets/js/jquery.app.js"></script>
